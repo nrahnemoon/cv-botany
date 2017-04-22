2bd10401dae936b1cd0be9197af5042755958490
nrahnemoon <nrahnemoon@gmail.com>
2017-04-22 16:34:23 -0400

    Plant image show

--- PlantImage.py
+++ PlantImage.py
@@ -7,9 +7,12 @@ class PlantImage:
 		self.path = imagePath
 		self.name = PlantUtils.PlantUtils.getImageNameFromPath(imagePath)
 		self.date = PlantUtils.PlantUtils.getDateFromImageName(self.name)
-		self.img = cv2.imread(imagePath, 0)
+		self.img = cv2.imread(imagePath, cv2.IMREAD_COLOR)
 
 	def show(self):
-		cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+		cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+		cv2.startWindowThread()
 		cv2.imshow(self.name, self.img)
 		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+		cv2.waitKey(1)
